588a28300a5b4bc53bd67b3b27bd8414db8312ba
root <root@swimmingpool.>
2024-11-14 22:46:50 +0800

    123

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 // app/layout.tsx
 import {Providers} from "./providers";
+import type { Viewport } from 'next';
+
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    minimumScale: 1,
+    userScalable: false,
+};
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
