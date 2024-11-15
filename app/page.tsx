831b1e9507deafb02f528ae352c25f43fd976fbd
root <root@swimmingpool.>
2024-11-15 22:55:43 +0800

    123

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex-col  justify-center">
+    <div className="grid justify-center">
         <Header />
         <div className="flex justify-center">
              <div className="bg-white">
