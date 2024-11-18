f0f941819f9b4decf286fd58b2eb0ddefd675453
ji.wu <ji.wu@nio.com>
2024-11-18 22:10:36 +0800

    支持

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function Home() {
   return (
     <div className=" grid grid-cols-1 justify-center">
-        <Header className="p-0" />
+        <Header />
         <div className="flex justify-center">
              <div className="bg-white">
                  <Link href="/hello" className="pl-3">
