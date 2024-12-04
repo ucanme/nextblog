b4fbd2b9e1baf31f8056c3c8ec706353a85b3ce0
root <root@swimmingpool.>
2024-12-04 23:01:17 +0800

    123

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -111,7 +111,7 @@ export default function Post({ code,sourceMd }: PostProps) {
 //     }
 // }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
     // slug
 
     // let tocElement
