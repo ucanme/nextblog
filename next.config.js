96167da8cf3d3ac9e5a19beb493b3e2bfde5301e
root <root@swimmingpool.>
2024-12-06 00:00:19 +0800

    123 ;

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig= {
     },
     distDir: "build",
     transpilePackages: ['@code-hike/mdx'],
+    webpack: (config) => {
+        config.externals.push('@code-hike/mdx');
+        return config;
+    },
 }
 
 module.exports = nextConfig
